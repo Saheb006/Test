a12d72084cd1e8fd1df8a389142fa17a46d4a255
Saheb-Sen <officialsaheb06@gmail.com>
2025-11-17 23:31:46 +0530

    added console.log (modifying test)

--- one/index.js
+++ one/index.js
@@ -4,3 +4,6 @@
 //Git remote add origin <repository_url> links the local repository to a remote repository --4
 //Git push -u origin main pushes the local commits to the remote repository on the main branch --5
 //Git log shows the commit history --6
+
+
+console.log("This is the index file in the one directory.");
