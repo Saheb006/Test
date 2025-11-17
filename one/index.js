56a0c480c6e12bbadaf213eeba5a659df41f4a0a
Saheb-Sen <officialsaheb06@gmail.com>
2025-11-17 23:13:39 +0530

    test

---
+++ one/index.js
@@ -0,0 +1,6 @@
+//Git init initializes git --1
+//Git add . adds all files to staging --2
+//Git commit -m "Initial commit" creates the first commit --3
+//Git remote add origin <repository_url> links the local repository to a remote repository --4
+//Git push -u origin main pushes the local commits to the remote repository on the main branch --5
+//Git log shows the commit history --6
